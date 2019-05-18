255da1e27de663ac22f57821cfa2f15eac4a9b2a
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-18 18:42:05 +0300

    rewite to es6 classes

--- app.js
+++ app.js
@@ -1,59 +1,55 @@
-//Book Constructor - для созданием объекта книги
-function Book(title, author, isbn){
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-};
-
-//UI Constructor - набор прототипных методов для добавление книги в библиотеку, удаление из библиотеки, алерты
-function UI(){};
-
-//add book to list
-UI.prototype.addBookToList = function(book){
-  const bookList = document.getElementById('book-list');
-  //create tr element
-  const row = document.createElement('tr');
-
-  //insert cols
-  row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete"> X </a></td>`;
-
-  bookList.appendChild(row);
-}
-
-//Clear fields
-UI.prototype.clearFields = function(){
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
-}
-
-//Show alert
-UI.prototype.showAlert = function(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert ${className}`;
-  div.appendChild(document.createTextNode(message));
-  //get parent
-  const container = document.querySelector('.container');
-  const form = document.getElementById('book-form');
-  //insert alert
-  container.insertBefore(div, form);
-
-  //remove alert
-  setTimeout(function(){
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
-
-//Delete book
-UI.prototype.deleteBook = function(target){
-  if (target.className === 'delete'){
-    target.parentElement.parentElement.remove();
+class Book{
+  constructor(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
 }
+
+class UI{
+  addBookToList(book){
+    const bookList = document.getElementById('book-list');
+    //create tr element
+    const row = document.createElement('tr');
+
+    //insert cols
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="delete"> X </a></td>`;
+
+    bookList.appendChild(row);
+  };
+
+  showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.container');
+    const form = document.getElementById('book-form');
+    //insert alert
+    container.insertBefore(div, form);
+
+    //remove alert
+    setTimeout(function(){
+      document.querySelector('.alert').remove();
+    }, 3000);
+  };
+
+  deleteBook(target){
+    if (target.className === 'delete'){
+      target.parentElement.parentElement.remove();
+    }
+  };
+
+  clearFields(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  };
+};
 
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit',function(e){
@@ -70,10 +66,8 @@ document.getElementById('book-form').addEventListener('submit',function(e){
 
   //Validate
   if(title === '' || author === '' || isbn === ''){
-
     //error alert
     ui.showAlert('Ошибка. Заполните все поля.', 'error');
-
   } else {
     //add book to UI
     ui.addBookToList(book);
